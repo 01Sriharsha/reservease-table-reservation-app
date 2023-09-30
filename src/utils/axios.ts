5fb5fe31145a65bcb8e9d5fa908cf1f56e75ac07
01Sriharsha <harshavasu463@gmail.com>
2023-09-30 22:33:02 +0530

    axios update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-export const apiClient = axios.create({ baseURL: "http://localhost:3000/api" });
-// export const apiClient = axios.create({ baseURL: "https://dinetable.vercel.app/api" });
+// export const apiClient = axios.create({ baseURL: "http://localhost:3000/api" });
+export const apiClient = axios.create({ baseURL: "https://reservease.vercel.app/api" });
